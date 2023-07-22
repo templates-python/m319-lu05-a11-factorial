65cd4bae4cedfc0d0870030d9202b6c56f724ec6
Marcel Suter <marcel.suter@bzz.ch>
2023-07-22 07:41:37 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def main_while():
-    # Write your program here and remove the line "pass"
-    pass
+    iterations = int(input('Give a number: '))
+    count = 1
+    fact = 1
+    while count <= iterations:
+        fact = fact * count
+        count += 1
+    print(fact)
+
 
 def main_for():
-    # Write your program here and remove the line "pass"
-    pass
+    iterations = int(input('Give a number: '))
+    fact = 1
+    for count in range(1, iterations + 1):  # start from 1, and iterations+1 to ensure that upper bound is included
+        fact = fact * count
+    print(fact)
 
 
 if __name__ == '__main__':
